bb0ce8618527f530612289f43c9663e5992e91a9
akilaamala <58208420+akilaamala@users.noreply.github.com>
2021-05-15 00:01:04 +0200

    javascript1
    First draft

---
+++ script/main_itsi.js
@@ -0,0 +1,34 @@
+var textBlock = document.createElement("INPUT");
+	textBlock.setAttribute("type", "text");
+
+	function calculate (){
+		let throttleValue = document.getElementById('throttle-value').value;
+		let value = Number(throttleValue); 
+if (Number.isNaN(value)){
+	alert("enter a number");
+}else {
+	if (value > 100){
+
+		alert('value too high!');
+	}
+	else {
+		alert('engage');
+		document.getElementById('speed-value').value = value * 120;
+		document.getElementById('fuel-value').value = value * 0.5;
+	}
+}
+}
+
+function checkSupplies (){
+	let foodValue = document.getElementById('food-value').value;
+	let waterValue = document.getElementById('water-value').value;
+	let daysLeft = document.getElementById('days-left').value = Math.ceil (Number(foodValue) / 3);
+}
+
+function gravityConverter (){
+let mass1 = document.getElementById('planet-mass').value;
+let distance = document.getElementById('distance-value').value;
+let massEarth = Math.pow (5.972,24);
+let gCons = Math.pow (6.673,-11);
+document.getElementById('gravity-value').value = gCons * massEarth * Number (mass1) / (Number (distance) * Number (distance)); 
+}
